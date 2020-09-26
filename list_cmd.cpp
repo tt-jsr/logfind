efa0d44860b828d54c5479a2dab1630d98fb9c9d
Jeff Richards <therichards.jeff@gmail.com>
2020-09-25 19:36:58 -0500

    list summary changes

--- list_cmd.cpp
+++ list_cmd.cpp
@@ -36,20 +36,20 @@ namespace logfind
                     std::cout << fi.filepath << std::endl << "     " << fi.sTimestamp << "  ->  " << fi.srotatetime;
                     if (fi.filepath.find(".gz") != std::string::npos)
                     {
-                        totalCompressed += fi.size/1000000;
-                        totalUncompressed += (fi.size*20)/1000000;
+                        totalCompressed += fi.size;
+                        totalUncompressed += fi.size;
                         std::cout << " (" << hours << "h " << min << "m " << secs << "s) " << fi.size/1000000 << "/" << (fi.size*20)/1000000 << " MB" << std::endl;
                     }
                     else // uncompressed rotated log
                     {
-                        totalUncompressed += (fi.size*20)/1000000;
+                        totalUncompressed += fi.size;
                         std::cout << " (" << hours << "h " << min << "m " << secs << "s) " << fi.size/1000000 << " MB" << std::endl;
                     }
                 }
             }
             else // unrotated log
             {
-                totalUncompressed += (fi.size*20)/1000000;
+                totalUncompressed += fi.size;
                 if (timestamp == 0 || timestamp > fi.timestamp)
                     std::cout << fi.filepath << std::endl << "     " << fi.sTimestamp << " " << fi.size/1000000 << " MB" << std::endl;
             }
@@ -69,8 +69,21 @@ namespace logfind
             int months, days, hours, min, secs;
             duration(diff, nullptr, &months, &days, &hours, &min, &secs, nullptr);
 
+            const char *suffix = " MB";
+            if (totalCompressed > 1E9)
+            {
+                totalCompressed /= 1E9;
+                totalUncompressed /= 1E9;
+                suffix = " GB";
+            }
+            else
+            {
+                totalCompressed /= 1E6;
+                totalUncompressed /= 1E6;
+            }
             std::cout << "    " << first.sTimestamp << "  ->  " << last.srotatetime;
-            std::cout << " (" << months << "mon " << days << "days " << hours << "h " << min << "m " << secs << "s) " << totalCompressed << "/" << totalUncompressed << " MB" << std::endl;
+            std::cout << " (" << months << "mon " << days << "days " << hours << "h " << min << "m " << secs << "s) " 
+                << totalCompressed << "/" << totalUncompressed << suffix << std::endl;
         }
         return;
     }
