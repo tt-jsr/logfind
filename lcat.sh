ef4a32e3f0ded9eea4cd82d35860cf439bf7c7a0
Jeff Richards <therichards.jeff@gmail.com>
2020-09-26 11:43:21 -0500

    launch script changes

--- lcat.sh
+++ lcat.sh
@@ -2,5 +2,10 @@
 
 run=`ls /opt/debesys/**/run`
 run=${run%% *}  # May have multiple runs. Pick the first one
+if [[ -z $LOGFIND_FILE ]]
+then
+    echo "LOGFIND_FILE is not defined"
+    exit 1
+fi
 $run ~/logfind cat $LOGFIND_FILE "$@"
 
