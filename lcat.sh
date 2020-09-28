b73fcc07cf8803bd9887024801e5a150c9e9e5a0
Jeff Richards <therichards.jeff@gmail.com>
2020-09-28 10:51:58 -0500

    Changed lauch script env variable

--- lcat.sh
+++ lcat.sh
@@ -2,10 +2,10 @@
 
 run=`ls /opt/debesys/**/run`
 run=${run%% *}  # May have multiple runs. Pick the first one
-if [[ -z $LOGFIND_FILE ]]
+if [[ -z $LOGFIND_LOGNAME ]]
 then
-    echo "LOGFIND_FILE is not defined"
+    echo "LOGFIND_LOGNAME is not defined"
     exit 1
 fi
-$run ~/logfind cat $LOGFIND_FILE "$@"
+$run ~/logfind cat $LOGFIND_LOGNAME "$@"
 
